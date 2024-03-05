cbcc29dcf8ad6394de89a8fdb4986d2d74d7b94b
Marcel Epp <epp.marcel@icloud.com>
2024-03-05 14:52:22 +0100

    add advanced search

--- components/Form.js
+++ components/Form.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import PreviewCard from "./PreviewCard";
 import SpotlightCard from "./SpotlightCard";
 
@@ -5,10 +6,11 @@ export default function Form({
   formResults,
   setFormResults,
   handleResults,
+  getUniqueRegions,
   getUniqueActivities,
   places,
   randomSurprise,
-
+  setRandomSurprise,
   handleSurprise,
 }) {
   // Function to get the user inputs from form
@@ -18,18 +20,45 @@ export default function Form({
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
 
-    const results = places.filter((place) => place.activity === data.activity);
+    let results = [];
+
+    if (data.activity && data.region === "disabled") {
+      results = places.filter((place) => place.activity === data.activity);
+    } else if (data.region && data.activity === "disabled") {
+      results = places.filter((place) => place.region === data.region);
+    } else if (data.region && data.activity) {
+      results = places.filter(
+        (place) =>
+          place.region === data.region && place.activity === data.activity
+      );
+    }
+
+    if (results.length === 0) {
+      results = -1;
+    }
+
     handleResults(results);
   }
 
   // handle form reset
   function handleFormReset() {
     setFormResults(0);
+    setRandomSurprise(0);
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <StyledForm onSubmit={handleSubmit}>
+        <label htmlFor="region">Choose your region</label>
+        <select name="region" id="region">
+          <option value="disabled">---</option>
+          {getUniqueRegions().map((region, id) => (
+            <option key={id} value={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+
         <label htmlFor="activity">Choose your activity</label>
         <select name="activity" id="activity">
           <option value="disabled">---</option>
@@ -39,6 +68,7 @@ export default function Form({
             </option>
           ))}
         </select>
+
         <button type="submit">Submit</button>
         <button type="reset" onClick={handleFormReset}>
           Reset
@@ -46,13 +76,19 @@ export default function Form({
         <button type="button" onClick={handleSurprise}>
           Surprise me
         </button>
-      </form>
-
-      {formResults.length > 0 && <PreviewCard formResults={formResults} />}
+      </StyledForm>
 
       {randomSurprise ? (
         <SpotlightCard randomSurprise={randomSurprise} />
       ) : null}
+      {formResults.length > 0 && <PreviewCard formResults={formResults} />}
+      {formResults === -1 && <h2>no matches...</h2>}
     </>
   );
 }
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
